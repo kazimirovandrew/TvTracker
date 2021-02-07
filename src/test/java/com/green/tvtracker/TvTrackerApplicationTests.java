e9b9f5b34b89e5fd4805733d4f136df7fddfd090
Andrew <kazimirovandrew@gmail.com>
2021-02-07 18:54:27 +0200

    Init commit

---
+++ src/test/java/com/green/tvtracker/TvTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.tvtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
